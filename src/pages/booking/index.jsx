2540d8bddee7027acd4c5378b00736e7c63bc722
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-09-15 11:31:29 +0700

    Update booking

--- src/pages/booking/index.jsx
+++ src/pages/booking/index.jsx
@@ -1,11 +1,183 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Checkbox,
+  DatePicker,
+  Space,
+} from "antd";
 
+// ------------------------------------------------------------------------------------------------
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const { Option } = Select;
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+const options = [
+  {
+    label: "Apple",
+    value: "Apple",
+  },
+  {
+    label: "Pear",
+    value: "Pear",
+  },
+  {
+    label: "Orange",
+    value: "Orange",
+  },
+];
+const onChange = (checkedValues) => {
+  console.log("checked = ", checkedValues);
+};
+const onChange1 = (value, dateString) => {
+  console.log("Selected Time: ", value);
+  console.log("Formatted Selected Time: ", dateString);
+};
+
+const onOk = (value) => {
+  console.log("onOk: ", value);
+};
+// ------------------------------------------------------------------------------------------------
 const BookingPage = () => {
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+84</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
   return (
-    <div className=''>
-        <div className=""></div>
-    </div>
-  )
-}
+    <div className="my-10 ">
+      <div className="w-[700px] m-auto">
+        <div className="">
+          <h1 className="text-4xl font-bold">Đặt lịch</h1>
+          <div className="border border-current rounded p-10 ">
+            <div className="mx-5">
+              <Form
+                {...layout}
+                name="nest-messages"
+                validateMessages={validateMessages}
+                initialValues={{
+                  prefix: "+84",
+                }}
+              >
+                <Form.Item
+                  name={["user", "name"]}
+                  label="Tên "
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  name={["user", "email"]}
+                  label="Email"
+                  rules={[
+                    {
+                      required: true,
+                      type: "email",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  name="phone"
+                  label="Số điện thoại"
+                  rules={[
+                    {
+                      required: true,
+                      type: "phone",
+                      message: "Please input your phone number!",
+                    },
+                  ]}
+                >
+                  <Input
+                    addonBefore={prefixSelector}
+                    style={{
+                      width: "100%",
+                    }}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name={["user", "age"]}
+                  label="Tuổi"
+                  rules={[
+                    {
+                      type: "number",
+                      min: 0,
+                      max: 99,
+                    },
+                  ]}
+                >
+                  <InputNumber />
+                </Form.Item>
 
-export default BookingPage
+                <Form.Item
+                  name={["user", "oders"]}
+                  label="Lựa chọn dịch vụ"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Checkbox.Group options={options} onChange={onChange} />
+                </Form.Item>
+
+                <Form.Item
+                  name={["user", "date"]}
+                  label="Chọn ngày"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <DatePicker showTime onChange={onChange1} onOk={onOk} />
+                </Form.Item>
+
+                <Form.Item name={["user", "introduction"]} label="Ghi chú">
+                  <Input.TextArea />
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                  <Button type="primary" htmlType="submit">
+                    Đặt lịch
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingPage;
