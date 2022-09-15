f97684d9757c8c73bb50cc1a3831ec85d8a030c6
Le Vu <levu08072002@gmail.com>
2022-09-16 00:00:09 +0700

    update css

--- src/components/Client/Footer/index.jsx
+++ src/components/Client/Footer/index.jsx
@@ -4,51 +4,46 @@ const Footers = () => {
   return (
     <>
       <div>
-        <div className="background bg-[#01321f]">
-          <div className=" w-[1920px] m-auto grid grid-cols-3 pt-10 pb-10 gap-[20px]  ">
-            <div className="w-[620px] px-[20px] text-center ">
+        <div className="background bg-[#01321f] pl-[170px] pr-[80px]">
+          <div className="max-w-full m-auto grid grid-cols-3 pt-[20px] gap-[20px]  ">
+            <div className=" px-[20px] text-center ">
               <div>
-                <img src="https://beautyspa4.shostweb.com/wp-content/uploads/2021/12/logo-spa-4.svg" alt="" />
+                <img src="https://beautyspa4.shostweb.com/wp-content/uploads/2021/12/logo-spa-4.svg" alt="" width={180} />
               </div>
-              <p className="pt-[10px] leading-5 text-[#FFFFFF] text-[20px]">Một sản phẩm của NinhbinhWeb</p>
-              <p className=" leading-5 text-[#FFFFFF]  text-[20px]">Tặng chúng tôi 1 ly cafe Ban Mê nhé!</p>
-
+              <p className="pt-[10px] leading-5 text-[#FFFFFF] text-[16px]">Một sản phẩm của NinhbinhWeb</p>
+              <p className=" leading-5 text-[#FFFFFF]  text-[16px]">Tặng chúng tôi 1 ly cafe Ban Mê nhé!</p>
             </div>
-            <div className="w-[620px] px-[20px]   ">
-              <h4 className="text-[#FFFFFF] ">CÔNG TY CỔ PHẦN THẨM MỸ THU CÚC</h4>
-              <p className="text-[20px] text-[#FFFFFF] text-center">BỆNH VIỆN THẨM MỸ THU CÚC</p>
-              <p className="text-[20px] text-[#FFFFFF] text-center">1B YẾT KIÊU, HAI BÀ TRƯNG, HÀ NỘI</p>
-              <p className="text-[20px] text-[#FFFFFF]">Tổng đài: 1900 1920</p>
-              <p className="text-[20px] text-[#FFFFFF]">Hotline: 0964 080 999</p>
-              <p className="text-[20px] text-[#FFFFFF]">contact@thucucclinics.vn</p>
-              <p className="text-[20px] text-[#FFFFFF]">Thẩm Mỹ Thu Cúc</p>
-              <p className="text-[20px] text-[#FFFFFF]">thucuc.tm (Thẩm Mỹ Thu Cúc)</p>
-
-
+            <div className="mr-5">
+              <h4 className="text-[#FFFFFF] text-[17px] ">CÔNG TY CỔ PHẦN THẨM MỸ THU CÚC</h4>
+              <p className="text-[16px] text-[#FFFFFF] text-center">BỆNH VIỆN THẨM MỸ THU CÚC</p>
+              <p className="text-[16px] text-[#FFFFFF] text-center">1B YẾT KIÊU, HAI BÀ TRƯNG, HÀ NỘI</p>
+              <p className="text-[13px] text-[#FFFFFF]">Tổng đài: 1900 1920</p>
+              <p className="text-[13px] text-[#FFFFFF]">Hotline: 0964 080 999</p>
+              <p className="text-[13px] text-[#FFFFFF]">contact@thucucclinics.vn</p>
+              <p className="text-[13px] text-[#FFFFFF]">Thẩm Mỹ Thu Cúc</p>
+              <p className="text-[13px] text-[#FFFFFF]">thucuc.tm (Thẩm Mỹ Thu Cúc)</p>
             </div>
 
-            <div className="w-[620px] px-[20px] ">
+            <div className=" px-[16px] mr-5 ">
               <h4 className="text-[#FFFFFF]">HỆ THỐNG THẨM MỸ THU CÚC</h4>
               <p className="text-[25px] text-[#E7C55A]">TẠI HÀ NỘI</p>
               <div>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">52 Lý Thường Kiệt, Hoàn Kiếm</p>
+                <p className="text-[16px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
+                <p className="text-[13px] text-[#FFFFFF] px-[40px]">52 Lý Thường Kiệt, Hoàn Kiếm</p>
               </div>
               <div>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">70 Cầu Giấy, Quận Cầu Giấy</p>
+                <p className="text-[16px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
+                <p className="text-[13px] text-[#FFFFFF] px-[40px]">70 Cầu Giấy, Quận Cầu Giấy</p>
               </div>
               <div>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">136 Nguyễn Trãi, Thanh Xuân</p>
+                <p className="text-[16px] text-[#FFFFFF] px-[40px]">THU CÚC MEGA BEAUTY CENTER</p>
+                <p className="text-[13px] text-[#FFFFFF] px-[40px]">136 Nguyễn Trãi, Thanh Xuân</p>
               </div>
               <p className="text-[25px] text-[#E7C55A]">TẠI TP.HCM</p>
               <div>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">55A – Đường 3/2 – Phường 11</p>
-                <p className="text-[20px] text-[#FFFFFF] px-[40px]">Quận 10 – TP. Hồ Chí Minh</p>
+                <p className="text-[16px] text-[#FFFFFF] px-[40px]">55A – Đường 3/2 – Phường 11</p>
+                <p className="text-[13px] text-[#FFFFFF] px-[40px]">Quận 10 – TP. Hồ Chí Minh</p>
               </div>
-
-
             </div>
 
           </div>
