29874a9ec446567ff7588d9953cf47df5be78563
tamltph13479 <thetam1002@gmail.com>
2022-09-15 22:35:57 +0700

    Creat-header

--- src/components/Client/Header/index.jsx
+++ src/components/Client/Header/index.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import 'react-slideshow-image/dist/styles.css';
+import Link from 'next/link'
 const Headers = () => {
   return (
     <>
-
       <div className="">
-        <div className=" bg-[#005E2E] ">
+        <div className="  bg-[#005E2E] ">
           <header className=" h-[84px] w-[1920px]  m-auto   ">
             <div>
               <div className="menu m-auto ">
                 <div>
                   <figure className="">
-                    <a href="">
+                    <Link href={'/'}>
                       <p><img src="https://beautyspa4.shostweb.com/wp-content/uploads/2021/12/logo-spa-4.svg" width="200px" className="ml-[450px]" /></p>
-                    </a>
+                    </Link>
                   </figure>
                 </div>
                 <div>
-                  <ul className=" pl-10 pt-[5px]">
-                    <a href="" ><button className="px-[23px] text-[#fff]" >Trang chủ</button></a>
-                    <a href=""><button className="px-[23px] text-[#fff] ">Sản phẩm</button></a>
-                    <a href=""><button className="px-[23px] text-[#fff]">Hỗ trợ</button></a>
-                    <a href=""><button className="px-[23px] text-[#fff]">Hỗ trợ</button></a>
-                    <a href=""><button className="px-[23px] text-[#fff]">Hỗ trợ</button></a>
-                    <a href=""><button className="px-[23px] text-[#fff]">Hỗ trợ</button></a>
+                  <ul className=" pl-10 pt-[25px]">
+                    <Link href={"/"} ><button className="px-[23px] text-[#fff]" >Trang chủ</button></Link>
+                    <Link href={`/products`}><button className="px-[23px] text-[#fff] ">Sản phẩm</button></Link>
+                    <Link href={"/"}><button className="px-[23px] text-[#fff]">Hỗ trợ</button></Link>
+                    <Link href={"/"}><button className="px-[23px] text-[#fff]">Hỗ trợ</button></Link>
+                    <Link href={"/"}><button className="px-[23px] text-[#fff]">Hỗ trợ</button></Link>
+                    <Link href={"/"}><button className="px-[23px] text-[#fff]">Hỗ trợ</button></Link>
                   </ul>
                 </div>
                 <div>
-                  <div className="pl-10 flex pt-[15px]">
-                    <a href="" className="rounded-md bg-[#003C21] text-[#fff] px-[45px] py-[5px] border-2 border-emerald-500"><button className="">0977595350</button></a>
+                  <div className="">
+                    <Link href={"/"} ><button className="rounded-md bg-[#003C21] text-[#fff] border-2 border-emerald-500">Đặt Lịch</button></Link>
                   </div>
                 </div>
 
@@ -42,7 +42,6 @@ const Headers = () => {
 
 
       </div>
-
     </>
   )
 };
