4df566c4e0c0efe096a06393eef0802e4a3699b7
quan08 <113536053+DucQuan88@users.noreply.github.com>
2022-09-23 22:30:47 +0700

    e-create-ui-admin-sidebar-TKW-[52]

--- src/components/Admin/Sidebar/index.js
+++ src/components/Admin/Sidebar/index.js
@@ -1,7 +1,43 @@
-import React from "react";
-
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import {Layout, Menu } from 'antd';
+import React, { useState } from 'react';
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { Header, Content, Footer, Sider } = Layout;  
+ 
+  const logo = {
+    height: "32px",
+    margin: "16px",
+    background: "rgba(255, 255, 255, 0.3)",
+  }
+  const [collapsed, setCollapsed] = useState(false);
+
+  function getItem(label, key, icon, children) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    };
+  }
+  const items = [
+    getItem('Services', '1', <PieChartOutlined />),
+    getItem('Shift', '2', <DesktopOutlined />),
+    getItem('Employee ', '3', <UserOutlined />),
+    getItem('User ', '4', <UserOutlined />),
+    // getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Contact', '5', <FileOutlined />),
+  ];
+
+  return <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+    <div style={logo} className="logo" />
+    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+  </Sider>;
 };
 
 export default Sidebar;
