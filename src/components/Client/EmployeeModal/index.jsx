82c05954efaf8c55f8d0b6110c3f5bec2ab37976
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-09-23 22:30:15 +0700

    TKW-51 <update>

--- src/components/Client/EmployeeModal/index.jsx
+++ src/components/Client/EmployeeModal/index.jsx
@@ -1,84 +1,86 @@
-import React, { useEffect } from 'react'
-import { Button, Modal } from 'antd';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import { Button, Modal, Radio } from "antd";
+import { useState } from "react";
 import useBooking from "../../../hooks/use-booking";
 import useEmployees from "../../../hooks/use-employees";
-import useSWR from 'swr';
-import moment from 'moment';
+import useSWR from "swr";
+import moment from "moment";
+
 const EmployeeModal = (props) => {
-    // const { data: bookings, error, getEmployeeByBookingDays } = useBooking();
-    const { data: employees, error } = useEmployees();
-    // const { data: employees, error } = useSWR(`http://localhost:5000/api/employees/${id}`);
-    // const data = getEmployeeByBookingDays('1663806900', '632a7d5b05c4f6b245410bdb')
+  const { data: bookings, getEmployeeByBookingDays } = useBooking();
+  const { data: employees, error } = useEmployees();
+  // const { data: employees, error } = useSWR(`http://localhost:5000/api/employees/${id}`);
+  // const data = getEmployeeByBookingDays('1663806900', '632a7d5b05c4f6b245410bdb')
 
+  console.log(employees.filter((item) => item._id == props.id));
 
-    console.log(employees.filter((item) => item._id == props.id));
+  const data = employees.filter((item) => item._id === props.id);
 
-    const data = employees.filter((item) => item._id === props.id)
-    // -------------------------------
-    const opens = props.open
-    const [open, setOpen] = useState(false);
-    const showModal = () => {
-        setOpen(true);
-    };
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const handleOk = () => {
+  // -------------------------------
+  const opens = props.open;
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+    getEmployeeByBookingDays();
+  };
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
-        setConfirmLoading(true);
-        setTimeout(() => {
-            setOpen(false);
-            setConfirmLoading(false);
-        }, 1000);
-    };
+  const [shiflt, setShiflt] = useState();
+  const onChange = (e) => {
+    setShiflt(e.target.value);
+    // console.log(`radio checked: ${e.target.value}`);
+  };
+  const handleOk = () => {
+    console.log(shiflt);
+    // props.onAdd(shiflt);
+  };
+  const handleCancel = () => {
+    // console.log('Clicked cancel button');
+    setOpen(false);
+  };
 
-    const handleCancel = () => {
-        // console.log('Clicked cancel button');
-        setOpen(false);
-    };
-
-    if (error) return <div>failed to load</div>;
-    if (!employees)
-        return (
-            <div>
-                Loading...
+  if (error) return <div>failed to load</div>;
+  if (!employees) return <div>Loading...</div>;
+  return (
+    <div>
+      <Button type="primary" onClick={showModal}>
+        Open Modal with async logic
+      </Button>
+      <Modal
+        title="Title"
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+      >
+        <div className="">
+          {data?.map((item) => (
+            <div className="" key={item._id}>
+              {/* <div className="">{item.name}</div>
+              <div className="">{item.email}</div>
+              <div className="">{item.phoneNumber}</div>
+              <div className="">{item.avatar}</div> */}
+              <div className="">
+                {item.timeWork?.map((item2) => (
+                  <div className="" key={item2._id}>
+                    <Radio.Group onChange={onChange} defaultValue="a">
+                      <Radio.Button value={item2.shiftId}>
+                        {item2.shiftId}
+                      </Radio.Button>
+                    </Radio.Group>
+                    {/* <div className="">
+                      {item2.date}-----{moment(item2.date).format("L")}
+                    </div>
+                    <div className="">{item2.shiftId}</div> */}
+                  </div>
+                ))}
+              </div>
             </div>
-        );
-    return (
-        <div>
-            <Button type="primary" onClick={showModal}  >
-                Open Modal with async logic
-            </Button>
-            <Modal
-                title="Title"
-                open={open}
-                onOk={handleOk}
-                confirmLoading={confirmLoading}
-                onCancel={handleCancel}
-                centered
-            >
-                <div className="">
+          ))}
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
-                    <div className="">{props.id}</div>
-                    {data?.map((item) => (
-                        <div className="" key={item._id}>
-                            <div className="">{item.name}</div>
-                            <div className="">{item.email}</div>
-                            <div className="">{item.phoneNumber}</div>
-                            <div className="">{item.avatar}</div>
-                            <div className="">{item.timeWork?.map((item2) => (
-                                <div className="" key={item2._id}>
-                                    <div className="">{item2.date}-----{moment(item2.date).format('L')}</div>
-                                    <div className="">{item2.shiftId}</div>
-
-                                </div>
-                            ))}</div>
-
-                        </div>
-                    ))}
-                </div>
-
-            </Modal></div>
-    )
-}
-
-export default EmployeeModal
+export default EmployeeModal;
--- src/hooks/use-booking.js
+++ src/hooks/use-booking.js
@@ -1,16 +1,16 @@
-
-import useSWR, { useSWRConfig } from 'swr';
-import instance from '../api/instance';
-import { getEmployeeByBookingDay } from '../api/client/booking';
+import useSWR, { useSWRConfig } from "swr";
+import instance from "../api/instance";
+import { getEmployeeByBookingDay } from "../api/client/booking";
 const useBooking = () => {
-
-    const { data, error, mutate } = useSWR(`/booking`);
-    const getEmployeeByBookingDays = async (query) => {
-        const get = await getEmployeeByBookingDay(query)
-        mutate(get)
-    }
-    return {
-        data, error, getEmployeeByBookingDays
-    }
-}
-export default useBooking
+  const { data, error, mutate } = useSWR(`/booking`);
+  const getEmployeeByBookingDays = async (query, employee) => {
+    const get = await getEmployeeByBookingDay(query, employee);
+    mutate(get);
+  };
+  return {
+    data,
+    error,
+    getEmployeeByBookingDays,
+  };
+};
+export default useBooking;
