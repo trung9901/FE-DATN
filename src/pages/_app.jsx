7914a39a92a0254db821436837cbb602b6f0c4c7
TranYen18 <tranyen18012001@gmail.com>
2022-09-24 08:00:52 +0700

    git commit -m "TKW-11  UI contact page"

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import LayoutMain from "./../components/Layout";
 import "antd/dist/antd.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function MyApp({ Component, pageProps }) {
   const LayoutWrapper = Component.Layout ?? LayoutMain;
